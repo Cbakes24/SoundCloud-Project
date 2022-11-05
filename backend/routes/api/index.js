86972ef89eb37468b539ace064beafa6eb52a5d3
Cory Baker <corybaker24@gmail.com>
2022-11-05 14:37:24 -0500

    Add User login backend endpoint session and users in API folder for endpoits

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
+// const { restoreUser } = require('../../utils/auth.js'); already pulling from the session router I think??
 const { requireAuth } = require('../../utils/auth.js');
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.get(
   '/restore-user',
@@ -14,9 +20,9 @@ router.get(
   }
 );
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
   router.get('/set-token-cookie', async (_req, res) => {
