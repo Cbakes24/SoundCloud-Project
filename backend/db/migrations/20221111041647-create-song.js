7df534962e4b270a91e423b51e5550af22437dee
Cory Baker <corybaker24@gmail.com>
2022-11-10 22:52:36 -0600

    heroku setup and created songs table

---
+++ backend/db/migrations/20221111041647-create-song.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('songs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      albumId: {
+        type: Sequelize.INTEGER
+      },
+      previewImage: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('songs');
+  }
+};
---
+++ backend/db/models/song.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class song extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  song.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    url: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    albumId: DataTypes.INTEGER,
+    previewImage: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'song',
+  });
+  return song;
+};
