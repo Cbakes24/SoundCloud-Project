461a867fe928c472b1df27ec9def3b6d00025c4d
Cory Baker <corybaker24@gmail.com>
2022-12-29 20:41:10 -0800

    added css to login

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,38 +1,42 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import './LoginForm.css'
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return (
-    <Redirect to="/" />
-  );
+  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
-    return dispatch(sessionActions.login({ credential, password }))
-      .catch(async (res) => {
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
         const data = await res.json();
         if (data && data.errors) setErrors(data.errors);
-      });
-  }
+      }
+    );
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className='loginDiv'>
+
+       <form onSubmit={handleSubmit}>
       <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
       </ul>
       <label>
         Username or Email
         <input
+          className="input"
           type="text"
           value={credential}
           onChange={(e) => setCredential(e.target.value)}
@@ -42,6 +46,7 @@ function LoginFormPage() {
       <label>
         Password
         <input
+          className="input"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -50,6 +55,8 @@ function LoginFormPage() {
       </label>
       <button type="submit">Log In</button>
     </form>
+    </div>
+
   );
 }
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,12 +1,12 @@
-import { csrfFetch } from './csrf';
+import { csrfFetch } from "./csrf";
 
-const SET_USER = 'session/setUser';
-const REMOVE_USER = 'session/removeUser';
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
 
 const setUser = (user) => {
   return {
     type: SET_USER,
-    payload: user,
+    payload: user
   };
 };
 
@@ -18,23 +18,31 @@ const removeUser = () => {
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
-  console.log(user, 'USER INFO')
-  const response = await csrfFetch('/api/session', {
-    method: 'POST',
+  console.log(user, "USER INFO");
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
     body: JSON.stringify({
       credential,
       password,
     }),
   });
   const data = await response.json();
-  console.log(data, 'DATTTTTAAAAAA')
-  console.log(data.user, 'PAYLOAD')
-  dispatch(setUser(data.user));
+  console.log(data, "DATTTTTAAAAAA");
+  console.log(data.user, "data.");
+  dispatch(setUser(data));
+  return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
   return response;
 };
 
 export const signup = (user) => async (dispatch) => {
-  const {firstName, lastName, username, email, password } = user;
+  const { firstName, lastName, username, email, password } = user;
   const response = await csrfFetch("/api/users", {
     method: "POST",
     body: JSON.stringify({
@@ -46,29 +54,28 @@ export const signup = (user) => async (dispatch) => {
     }),
   });
   const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
   dispatch(setUser(data.user));
   return response;
 };
 
-export const restoreUser = () => async dispatch => {
-    const response = await csrfFetch('/api/session');
-    const data = await response.json();
-    dispatch(setUser(data.user));
-    return response;
-  };
+const initialState = () => ({ user: null });
 
-const initialState = { user: null };
-
-const sessionReducer = (state = initialState, action) => {
-  let newState = {...state}
+const sessionReducer = (state = initialState(), action) => {
+  let newState = { ...state };
+  console.log(newState, 'NEWSTATE')
   switch (action.type) {
     case SET_USER:
-      newState.payload = action.user;
+      newState[action.user] = action.payload;
       return newState;
     case REMOVE_USER:
-      newState = Object.assign({}, state);
-      newState.user = null;
-      return newState;
+      return state;
     default:
       return state;
   }
