c561e9be0b6f9beab03724fb54481f94521a8c5e
Cory Baker <corybaker24@gmail.com>
2023-01-05 22:53:37 -0800

    submitting a new song takes you back to the songs list page

--- frontend/src/components/Songs/addSongForm.js
+++ frontend/src/components/Songs/addSongForm.js
@@ -1,87 +1,86 @@
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { createSong } from '../../store/songs'
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { createSong } from "../../store/songs";
 
+const AddSongForm = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-const AddSongForm = ()  => {
-const dispatch = useDispatch()
-const history = useHistory()
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setUrl] = useState("");
+  // const [userId, setUserId] = useState(`${user.id}`)
+  const [albumTitle, setAlbumTitle] = useState("");
+  const [image, setImage] = useState("");
 
-const [title, setTitle] = useState('')
-const [description, setDescription] = useState('')
-const [url, setUrl] = useState('')
-// const [userId, setUserId] = useState(`${user.id}`)
-const [albumTitle, setAlbumTitle] = useState('')
-const [image, setImage] = useState('')
-
-const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const payload = { //does the order of items matter
-        title,
-        description,
-        url,
-        image,
-        albumTitle //they wouldnt enter album id, if they enter album name does that work?
-    }
-
-    const newSong = await dispatch(createSong(payload))
-
-// history.push(`./songs/${song.id}`)
-}
-
-const handleCancelClick = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    history.push('./')
+
+    const payload = {
+      //does the order of items matter
+      title,
+      description,
+      url,
+      image,
+      albumTitle, //they wouldnt enter album id, if they enter album name does that work?
+    };
+
+    const newSong = await dispatch(createSong(payload));
+
+    history.push(`./`);
   };
 
-return (
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    history.push("./");
+  };
+
+  return (
     <section>
-         <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <h2>Add a Song Form</h2>
         <label>Song Name</label>
         <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}>
-         </input>
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
 
-         <label>Album Name</label>
+        <label>Album Name</label>
         <input
-        type="text"
-        value={albumTitle}
-        onChange={(e) => setAlbumTitle(e.target.value)}>
-         </input>
+          type="text"
+          value={albumTitle}
+          onChange={(e) => setAlbumTitle(e.target.value)}
+        ></input>
 
-         <label>Song Description</label>
+        <label>Song Description</label>
         <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}>
-         </input>
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></input>
 
-         <label>Song Link</label>
+        <label>Song Link</label>
         <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}>
-         </input>
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        ></input>
 
-         <label>Song Picture</label>
+        <label>Song Picture</label>
         <input
-        type="text"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}>
-         </input>
-         <button type="submit">Create new Song</button>
-        <button type="button" onClick={handleCancelClick}>Cancel</button>
-    </form>
-
+          type="text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        ></input>
+        <button type="submit">Create new Song</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </form>
     </section>
+  );
+};
 
-)
-
-}
-
-export default AddSongForm
+export default AddSongForm;
