596569d80a64ae3733ea150cf34062f9e72c7dbb
Cory Baker <corybaker24@gmail.com>
2023-01-16 15:01:02 -0800

    added links for git hub and linked in

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -14,53 +14,86 @@ function Navigation({ isLoaded, setIsLoaded }) {
   if (sessionUser) {
     sessionLinks = (
       <div className="container">
-          <NavLink activeClassName="active" exact to={`/songs/users/${sessionUser.id}`}>
-              My Songs
-          </NavLink>
+        <NavLink
+          activeClassName="active"
+          exact
+          to={`/songs/users/${sessionUser.id}`}
+        >
+          My Songs
+        </NavLink>
 
-          <li className="navList"> <ProfileButton user={sessionUser} /></li>
+        <li className="navList">
+          {" "}
+          <ProfileButton user={sessionUser} />
+        </li>
       </div>
     );
   } else {
     sessionLinks = (
-      <div className='container'>
-        <li><NavLink to="/login">Log In</NavLink></li>
-        <li><NavLink to="/signup">Sign Up</NavLink></li>
+      <div className="container">
+        <li>
+          <NavLink to="/login">Log In</NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </li>
       </div>
-
-
     );
   }
 
   return (
     <>
- <header id='main-header'>
-    <div className="'container">
-      <h1> JAM PACKED SOUND CLOUD</h1>
-    </div>
-  </header>
+      <header id="main-header">
+        <div className="'container">
+          <h1> JAM PACKED SOUND CLOUD</h1>
+          <li className="navList">
+            {" "}
+            <NavLink
+              activeClassName="active"
+              exact
+              to="https://github.com/Cbakes24/SoundCloud-Project"
+            >
+              GitHub
+            </NavLink>
+          </li>
+          <li className="navList">
+            {" "}
+            <NavLink
+              activeClassName="active"
+              exact
+              to="https://www.linkedin.com/in/cory-baker-9738ba2a/"
+            >
+              GitHub
+            </NavLink>
+          </li>
+        </div>
+      </header>
 
-    <nav id='navbar'>
-      <div className="container">
-        <ul>
-          <li className="navList"> <NavLink activeClassName="active" exact to="/">
-              Home
-            </NavLink></li>
-          <li className="navList"> <NavLink activeClassName="active" to="/comments">
-              Feed
-            </NavLink></li>
-          <li className="navList"><NavLink activeClassName="active" exact to="/songs">
-              Songs
-            </NavLink></li>
-          <li className="navList"> {isLoaded && sessionLinks}</li>
-
-
-
-        </ul>
-      </div>
-    </nav>
+      <nav id="navbar">
+        <div className="container">
+          <ul>
+            <li className="navList">
+              {" "}
+              <NavLink activeClassName="active" exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="navList">
+              {" "}
+              <NavLink activeClassName="active" to="/comments">
+                Feed
+              </NavLink>
+            </li>
+            <li className="navList">
+              <NavLink activeClassName="active" exact to="/songs">
+                Songs
+              </NavLink>
+            </li>
+            <li className="navList"> {isLoaded && sessionLinks}</li>
+          </ul>
+        </div>
+      </nav>
     </>
-
   );
 }
 
