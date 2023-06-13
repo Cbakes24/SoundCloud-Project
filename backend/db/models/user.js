88603e5e6fa15523c0adf668b34af9f6139e9570
Cory Baker <corybaker24@gmail.com>
2023-06-13 11:29:26 -0700

    moved static associations down the file before User.init

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,10 +1,9 @@
 "use strict";
-const bcrypt = require('bcryptjs');
-const { Model, Validator } = require('sequelize');
+const bcrypt = require("bcryptjs");
+const { Model, Validator } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-
-//instance method
+  //instance method
   class User extends Model {
     toSafeObject() {
       const { id, username, email, firstName, lastName } = this; // context will be the User instance
@@ -14,44 +13,34 @@ module.exports = (sequelize, DataTypes) => {
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
     }
- 
-    //static association
-    static associate(models) {
-      User.hasMany(models.Playlist,
-        { foreignKey: 'userId', onDelete: 'cascade', hooks: true  });
 
-      User.hasMany(models.Album,
-        { foreignKey: 'userId' });
-
-      User.hasMany(models.Song,
-        { foreignKey: 'userId'  });
-
-      User.hasMany(models.Comment,
-        { foreignKey: 'userId', onDelete: 'cascade', hooks: true });
-
-   
-    }
-    
     static getCurrentUserById(id) {
       return User.scope("currentUser").findByPk(id);
     }
 
     static async login({ credential, password }) {
-      const { Op } = require('sequelize');
-      const user = await User.scope('loginUser').findOne({
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
         where: {
           [Op.or]: {
             username: credential,
-            email: credential
-          }
-        }
+            email: credential,
+          },
+        },
       });
       if (user && user.validatePassword(password)) {
-        return await User.scope('currentUser').findByPk(user.id);
+        return await User.scope("currentUser").findByPk(user.id);
       }
     }
 
-    static async signup({ username, email, password, firstName, lastName, previewImage }) {
+    static async signup({
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+      previewImage,
+    }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
@@ -59,12 +48,30 @@ module.exports = (sequelize, DataTypes) => {
         hashedPassword,
         firstName,
         lastName,
-        previewImage
+        previewImage,
       });
-      return await User.scope('currentUser').findByPk(user.id);
+      return await User.scope("currentUser").findByPk(user.id);
     }
 
-  };
+    //static association
+    static associate(models) {
+      User.hasMany(models.Playlist, {
+        foreignKey: "userId",
+        onDelete: "cascade",
+        hooks: true,
+      });
+
+      User.hasMany(models.Album, { foreignKey: "userId" });
+
+      User.hasMany(models.Song, { foreignKey: "userId" });
+
+      User.hasMany(models.Comment, {
+        foreignKey: "userId",
+        onDelete: "cascade",
+        hooks: true,
+      });
+    }
+  }
 
   User.init(
     {
@@ -86,47 +93,47 @@ module.exports = (sequelize, DataTypes) => {
             if (Validator.isEmail(value)) {
               throw new Error("Cannot be an email.");
             }
-          }
-        }
+          },
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           len: [3, 256],
-          isEmail: true
-        }
+          isEmail: true,
+        },
       },
       hashedPassword: {
         type: DataTypes.STRING.BINARY,
         allowNull: false,
         validate: {
-          len: [60, 60]
+          len: [60, 60],
         },
       },
 
       previewImage: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
       },
-
     },
     {
       sequelize,
       modelName: "User",
       defaultScope: {
         attributes: {
-          exclude: ["hashedPassword", ,"email", "createdAt", "updatedAt"]
-        }
+          exclude: ["hashedPassword", , "email", "createdAt", "updatedAt"],
+        },
       },
       scopes: {
         currentUser: {
           attributes: {
-          exclude: ["hashedPassword"] }
+            exclude: ["hashedPassword"],
+          },
         },
         loginUser: {
-          attributes: {}
-        }
-      }
+          attributes: {},
+        },
+      },
     }
   );
   return User;
