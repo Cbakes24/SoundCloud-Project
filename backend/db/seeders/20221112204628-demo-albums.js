7f0167b37aa74b5f09a503b62ba50546dd05bc8c
Cory Baker <corybaker24@gmail.com>
2023-06-14 22:35:11 -0700

    added more album seeders

--- backend/db/seeders/20221112204628-demo-albums.js
+++ backend/db/seeders/20221112204628-demo-albums.js
@@ -16,28 +16,50 @@ module.exports = {
         title: 'Dookie',
         description: 'Best Green Day Album everrrr',
         userId: 1,
-        previewImage: 'img.com'
+        previewImage: 'https://upload.wikimedia.org/wikipedia/en/4/4b/Green_Day_-_Dookie_cover.jpg'
       },
       {
         title: 'Blood Sex Sugar magik',
         description: 'Come on Anthony gimmie some sugaa',
         userId: 2,
-        previewImage: 'img.com'
+        previewImage: 'https://upload.wikimedia.org/wikipedia/en/5/5e/RHCP-BSSM.jpg'
       },
       {
         title: 'Giving Kids to Candy',
         description: 'Forever changed the Rock and Roll World',
         userId: 3,
-        previewImage: 'img.com'
+        previewImage: 'https://m.media-amazon.com/images/I/51qQRA-hjzL._UXNaN_FMjpg_QL85_.jpg'
+      },
+      {
+        title: 'Aenima',
+        description: 'This whole allbum is such a vibe. Close your eye and FEEL!',
+        userId: 3,
+        previewImage: 'https://upload.wikimedia.org/wikipedia/en/2/2f/Aenima.jpg'
+      },
+      {
+        title: 'Flow State',
+        description: 'The instrumentals on this are unbelievable, nobody does it like ya girl TASHHHHH',
+        userId: 1,
+        previewImage: 'https://upload.wikimedia.org/wikipedia/en/d/d7/Tash_Sultana_Flow_State_Album_Artwork.png'
+      },
+      {
+        title: 'Inbetween Dreams',
+        description: 'The instrumentals on this are unbelievable, nobody does it like ya girl TASHHHHH',
+        userId: 2,
+        previewImage: 'https://e.snmc.io/i/600/s/35e6aed697966958afb3467184e2fc3c/2488293/jack-johnson-in-between-dreams-Cover-Art.jpg'
+      },
+      {
+        title: 'Silent Steeples',
+        description: 'The homies from New England created some of the most chill awesome music ever on this album',
+        userId: 1,
+        previewImage: 'https://f4.bcbits.com/img/a1562865730_10.jpg'
       },
     ]);
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     options.tableName = 'Albums'
-    const Op = Sequelize.Op;
-    await queryInterface.bulkDelete(options, null, {
-      title: ['Dookie', 'Blood Sex Sugar magik', 'Giving Kids to Candy']
-    });
+      await queryInterface.bulkDelete(options, null, {});
+
   }
 };
