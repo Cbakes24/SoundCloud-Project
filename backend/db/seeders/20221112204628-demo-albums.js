82a5d19eb268ced34e2805af61272e4f26416b3c
Cory Baker <corybaker24@gmail.com>
2023-06-22 16:12:26 -0700

    addded typ import to album seeder

--- backend/db/seeders/20221112204628-demo-albums.js
+++ backend/db/seeders/20221112204628-demo-albums.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Albums'
