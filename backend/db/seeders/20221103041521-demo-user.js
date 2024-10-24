7df6f0fd1526382f8648e3c10ce21de5d6990d04
Cory Baker <corybaker24@gmail.com>
2024-10-24 10:53:39 -0700

    consolelogs in user demo seeder

--- backend/db/seeders/20221103041521-demo-user.js
+++ backend/db/seeders/20221103041521-demo-user.js
@@ -2,53 +2,62 @@
 const { Album, Comment, Playlist, PlaylistSong, Song, User } = require('../models');
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA;  // Define your schema in options object
 }
 const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    options.tableName = 'Users'
-    await queryInterface.bulkInsert(options, [
-      {
-        firstName: 'Cory',
-        lastName: 'Baker',
-        email: 'cory@gmail.com',
-        username: 'corybaker24',
-        hashedPassword: bcrypt.hashSync('corysoloman'),
-        previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/goomba-pic.jpeg'
-      },
-   
-      {
-        firstName: 'Ayla',
-        lastName: 'Baker',
-        email: 'ayla@gmail.com',
-        username: 'aylarey',
-        hashedPassword: bcrypt.hashSync('aylarey'),
-        previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/luigi-pic.png'
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Users';
 
-      },
-      {
-        firstName: 'Leon',
-        lastName: 'Baker',
-        email: 'leon@gmail.com',
-        username: 'LeonLegend',
-        hashedPassword: bcrypt.hashSync('leonross'),
-        previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/mario-pic.png'
-      }
-    ], {returning: true});
-        console.log("Users created: ", users);  // Log the users to see if they exist
-  
+    // Insert users and return inserted rows
+    const users = await queryInterface.bulkInsert(
+      options,
+      [
+        {
+          firstName: 'Cory',
+          lastName: 'Baker',
+          email: 'cory@gmail.com',
+          username: 'corybaker24',
+          hashedPassword: bcrypt.hashSync('corysoloman'),
+          previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/goomba-pic.jpeg',
+        },
+        {
+          firstName: 'Ayla',
+          lastName: 'Baker',
+          email: 'ayla@gmail.com',
+          username: 'aylarey',
+          hashedPassword: bcrypt.hashSync('aylarey'),
+          previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/luigi-pic.png',
+        },
+        {
+          firstName: 'Leon',
+          lastName: 'Baker',
+          email: 'leon@gmail.com',
+          username: 'LeonLegend',
+          hashedPassword: bcrypt.hashSync('leonross'),
+          previewImage: 'https://corysoundcloudawsbucket.s3.us-west-1.amazonaws.com/mario-pic.png',
+        },
+      ],
+      { returning: true }  // Correct placement of this option
+    );
+
+    console.log("Users created: ", users);  // Log the users to see if they exist
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Users'
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
     const Op = Sequelize.Op;
-      await queryInterface.bulkDelete(options, 
-        {
-          username: {[Op.in]: ['corybaker24', 'aylarey', 'LeonLegend']}
-         }, {});
 
-  }
+    await queryInterface.bulkDelete(
+      options,
+      {
+        username: {
+          [Op.in]: ['corybaker24', 'aylarey', 'LeonLegend'],
+        },
+      },
+      {}
+    );
+  },
 };
