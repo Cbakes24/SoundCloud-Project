8402003ff2b6691a7783facc68605d49f7b82833
Cory Baker <corybaker24@gmail.com>
2024-10-29 23:31:10 -0700

    module.exports in function seeders

--- backend/db/seeders/seederFunctions.js
+++ backend/db/seeders/seederFunctions.js
@@ -1,13 +1,19 @@
 const { Album, Comment, Playlist, PlaylistSong, Song, User } = require('../models');
+const Sequelize = require('sequelize');  // Add this if you use Sequelize operators like Sequelize.Op
 
-export const users = await User.findAll({
-    where: {
-      username: { [Sequelize.Op.in]: ['corybaker24', 'aylarey', 'LeonLegend'] },
-    },
-    raw: true,  // Return plain data instead of Sequelize instances
-  });
+// Fetch users
+const users = await User.findAll({
+  where: {
+    username: { [Sequelize.Op.in]: ['corybaker24', 'aylarey', 'LeonLegend'] },
+  },
+  raw: true,  // Return plain data instead of Sequelize instances
+});
 
-  export const userMap = users.reduce((acc, user) => {
-    acc[user.username] = user.id;  // Map usernames to their corresponding IDs
-    return acc;
-  }, {});
+// Map usernames to their IDs
+const userMap = users.reduce((acc, user) => {
+  acc[user.username] = user.id;
+  return acc;
+}, {});
+
+// Export the values using CommonJS syntax
+module.exports = { users, userMap };
